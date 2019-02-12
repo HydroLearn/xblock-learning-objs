029c5464b1c54d4b157fac65e68c1d14b0037387
crivet <c.rivet@louisiana.edu>
2019-02-12 10:06:50 -0600

    - bugfixes - added initial styling skeleton for abet-selection and learning level/verb wrappers

--- hl_learning_objs/static/js/hl_learning_objs-lms.js
+++ hl_learning_objs/static/js/hl_learning_objs-lms.js
@@ -1,9 +1,16 @@
 /* JavaScript for HydroLearn's learning objectives XBlock, LMS Side. */
 function HL_LO_XBlock(runtime, xblock_element) {
-
+    // runtime code for loading the xblock in the LMS portion of the site
     $(function ($) {
-		// runtime code for loading the xblock in the LMS portion of the site
-		// 		i.e. the student view initalization
 
+        var catalog = new LO_catalog();
+
+        // import the data from the xblock_element into the catalog
+
+        // generate learning objective strings
+
+
+        // append the string list to the xblock's contents
+        $('.HL_LO_xblock').append('a list of strings for learning objectives')
     });
 }
