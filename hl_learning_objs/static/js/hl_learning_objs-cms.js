422b55ad14ea50855377e8fca4232901ce376abf
crivet <c.rivet@louisiana.edu>
2019-02-13 15:03:38 -0600

    testing parsing of learning_objs

--- hl_learning_objs/static/js/hl_learning_objs-cms.js
+++ hl_learning_objs/static/js/hl_learning_objs-cms.js
@@ -1,9 +1,10 @@
 /* JavaScript for HydroLearn's learning objectives XBlock, Studio Side. */
 function HL_LO_XBlockStudio(runtime, xblock_element) {
-    debugger;
+
     // add modal tag so it's width gets adjusted on window resize
     $(xblock_element).closest('.modal-window').addClass('hl_resize_correction');
 
+    // ''
     var catalog = new LO_catalog(JSON.parse('{{ blooms_catalog|safe }}'));
 
     // Define mapping of tabs (modes) to display names
@@ -15,10 +16,10 @@ function HL_LO_XBlockStudio(runtime, xblock_element) {
     function initialize_forms(){
         $('.action_input', xblock_element).append(catalog._generate_level_selection());
         $('.abet_input', xblock_element).append(catalog._generate_ABET_selection());
-        debugger;
-        // import this xblocks data into the catalog for use in the system
-        //catalog.import_objectives(JSON.parse('{{ self.learning_objs|safe }}'))
 
+        // import this xblocks data into the catalog for use in the system
+        //catalog.import_objectives(JSON.parse('{{ self.learning_objs|escapejs }}'))
+        debugger;
         // by default hide the creation form until called
         $('#learning_obj_creation', xblock_element).hide()
 
