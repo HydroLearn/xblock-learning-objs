2ae2c3f422da3a44b0cbb88bf482f64dcb486d38
crivet <c.rivet@louisiana.edu>
2019-02-14 16:18:33 -0600

    correcting method name

--- hl_learning_objs/hl_learning_objs.py
+++ hl_learning_objs/hl_learning_objs.py
@@ -104,7 +104,7 @@ class HL_LearningObjs_XBlock(XBlock):
         fragment.add_javascript(unicode(render_template('static/js/hl_learning_objs-lms.js', content)))
 
         fragment.initialize_js('LO_catalog')
-        fragment.initialize_js('HL_LO_XBlock', {'objs': self.learning_objs.as_json() })
+        fragment.initialize_js('HL_LO_XBlock', {'objs': self.learning_objs.to_json() })
 
 
         return fragment
