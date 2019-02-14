48267e21c4f011d4d925cab58653d32cf47946de
crivet <c.rivet@louisiana.edu>
2019-02-14 16:31:26 -0600

    attempting catalog setup on initialization of the lms xblock js

--- hl_learning_objs/static/js/hl_learning_objs-lms.js
+++ hl_learning_objs/static/js/hl_learning_objs-lms.js
@@ -2,19 +2,21 @@
 function HL_LO_XBlock(runtime, xblock_element, configuration) {
     // runtime code for loading the xblock in the LMS portion of the site
     debugger;
+    var catalog = new LO_catalog(JSON.parse('{{ blooms_catalog|safe }}'));
+
+    if(!!configuration.objs){
+        var existing = JSON.parse(configuration.objs);
+        catalog.import_records(existing);
+    }
+
+
+
     $(function ($) {
 
         debugger;
-        var catalog = new LO_catalog(JSON.parse('{{ blooms_catalog|safe }}'));
-
-        // parse the existing objects
-        var existing = JSON.parse('{{ objs|safe|escapejs }}');
-
-        var test_str = '{{ self.learning_objs }}';
-
 
         // import this xblocks data into the catalog for use in the system
-        catalog.import_records(existing)
+
 
         // add this listing of catalog records to the template
         $('.objectives_listing', xblock_element).html(catalog.records_as_html());
