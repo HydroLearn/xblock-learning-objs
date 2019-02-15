527047aaf7b86028fe39199667ba8a047f4f2000
crivet <c.rivet@louisiana.edu>
2019-02-15 10:26:52 -0600

    binding the proper event to abet input checkboxes... keyup? really? c'mon

--- hl_learning_objs/static/js/hl_learning_objs-cms.js
+++ hl_learning_objs/static/js/hl_learning_objs-cms.js
@@ -216,7 +216,7 @@ function HL_LO_XBlockStudio(runtime, xblock_element, viewbag) {
         $("#learning_obj_wizard", xblock_element).on('change','.learning_verb_selection', update_preview);
         $("#learning_obj_wizard", xblock_element).on('keyup', '#task', update_preview);
         $("#learning_obj_wizard", xblock_element).on('keyup', '#degree', update_preview);
-        $("#learning_obj_wizard", xblock_element).on('keyup', '.ABET_input', update_ABET_review);
+        $("#learning_obj_wizard", xblock_element).on('change', '.ABET_input', update_ABET_review);
     }
 
 
